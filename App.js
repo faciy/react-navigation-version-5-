419ab123edd7b2b8691628d2c6e9ea6b1b2eaeae
kouassicharlesothniel <kouassicharlesothniel@gmail.com>
2020-09-18 16:19:52 +0000

    deuxième commit

--- App.js
+++ App.js
@@ -4,8 +4,13 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import MainTabScreen from './screens/MainTabScreen';
 
+import MainTabScreen from './screens/MainTabScreen';
+import SupportScreen from './screens/SupportScreen';
+import SettingsScreen from './screens/SettingsScreen';
+import BookmarkScreen from './screens/BookmarkScreen';
+
+import {DrawerContent} from './screens/DrawerContent'
 
 const Drawer = createDrawerNavigator();
 
@@ -14,9 +19,11 @@ const Drawer = createDrawerNavigator();
 const App = () =>{
   return(
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />}>
         <Drawer.Screen name="Home" component={MainTabScreen} />
-        {/* <Drawer.Screen name="Details" component={DetailsStackScreen} /> */}
+        <Drawer.Screen name="SupportScreen" component={SupportScreen} />
+        <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
+        <Drawer.Screen name="BookmarkScreen" component={BookmarkScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
